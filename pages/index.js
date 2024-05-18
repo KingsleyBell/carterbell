d1c3a3bee00f6de9b0b732d9b38ca06882c3a957
Luke Bell <lukekingsleybell@gmail.com>
2024-05-18 11:29:30 +0100

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>CARTERBELL</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          CARTERBELL
         </h1>
 
         <p className={styles.description}>
